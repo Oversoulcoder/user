1b425953b7502354cccbeca8ace9fde6032edee0
Oversoulcoder <socatesmecaros@gmail.com>
2024-01-17 23:44:25 +0800

    first commit

---
+++ src/components/BlogCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const BlogCard = () => {
+  return (
+    <div className="col-3">
+      <div className="blog-card">
+        <div className="card-image">
+          <img src="images/blog-1.jpg" className="img-fluid" alt="blog" />
+        </div>
+        <div className="blog-content">
+          <p className="date">January 20, 2024</p>
+          <h5 className="title">A beautiful sunday morning renaissance</h5>
+          <p className="desc">
+            In the rapidly evolving landscape of technology, Virtual Reality
+            (VR) has emerged as a transformative force, offering immersive
+            experiences that redefine the way we interact with digital content.
+            At the forefront of this revolution is the VR headgear – a sleek and
+            futuristic device that opens doors to a world of limitless
+            possibilities. In this blog post, we will delve into the fascinating
+            realm of VR headgear, exploring its features, applications, and the
+            unparalleled experiences it brings to users.
+          </p>
+          <Link to="/" className="button">
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
