dda24ac9ddfaaace69d320dece5fd08eb3b0041c
Oversoulcoder <socatesmecaros@gmail.com>
2024-01-31 13:34:50 +0800

    ninth user update

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -16,10 +16,19 @@ const ProductCard = (props) => {
     <>
       <div
         className={` ${
-          location.pathname === "/store" ? `gr-${grid}` : "col-3"
+          location.pathname === "/product" ? `gr-${grid}` : "col-3"
         }`}
       >
-        <Link to=":id" className="product-card position-relative">
+        <Link
+          to={`(${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/1"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
           <div className="wishlist-icon position-absolute">
             <button className="border-0 bg-transparent">
               <img src={wish} alt="wishlist" />
@@ -67,10 +76,19 @@ const ProductCard = (props) => {
       </div>
       <div
         className={` ${
-          location.pathname === "/store" ? `gr-${grid}` : "col-3"
+          location.pathname === "/product" ? `gr-${grid}` : "col-3"
         }`}
       >
-        <Link className="product-card position-relative">
+        <Link
+          to={`(${
+            location.pathname == "/"
+              ? "/product/:id"
+              : location.pathname == "/product/:id"
+              ? "/product/:id"
+              : ":id"
+          }`}
+          className="product-card position-relative"
+        >
           <div className="wishlist-icon position-absolute">
             <Link>
               <img src={wish} alt="wishlist" />
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -183,11 +183,14 @@ const OurStore = () => {
                   <p className="mb-0 d-block" style={{ width: "100px" }}>
                     Sort By:
                   </p>
-                  <select name="" className="form-control form-select" id="">
+                  <select
+                    name=""
+                    defaultValue={"DEFAULT"}
+                    className="form-control form-select"
+                    id=""
+                  >
                     <option value="manual">Featured</option>
-                    <option value="best-selling" selected="selected">
-                      Best Selling
-                    </option>
+                    <option value="best-selling">Best Selling</option>
                     <option value="title-accending">Alphabetically, A-Z</option>
                     <option value="title-decending">Alphabetically, Z-A</option>
                     <option value="Price-accending">Price, Low to High</option>
